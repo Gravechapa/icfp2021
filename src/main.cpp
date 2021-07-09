0df8b787f8dadb5a09e5eea0d782ce06c1f02773
Leonid Olevskii <stuv@protonmail.com>
2021-07-09 16:33:03 +0300

    Add hello request

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,30 @@
+#include <cxxopts.hpp>
+#include <iostream>
+#include <httplib.h>
+
 int main(int argc, char* argv[])
 {
+    cxxopts::Options options("ICFPC2021", "Program for solving tasks");
+
+    options.add_options()
+        ("t,task", "Task id", cxxopts::value<int>())
+        ("token", "Access token", cxxopts::value<std::string>())
+        ("h,help", "Print usage");
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+    }
+
+    httplib::Client cli("https://poses.live/api");
+
+    httplib::Headers header = {
+        {"Authorization", "Bearer" + result["token"].as<std::string>()}
+    };
+
+    cli.Get("/hello", header);
+
     return 0;
 }
