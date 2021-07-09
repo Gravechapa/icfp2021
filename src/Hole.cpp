f2efec4076627d9afdb9921ec02528ef6f64f48b
Gravechapa <Gravechapa@protonmail.com>
2021-07-09 20:05:10 +0300

    Add visual output

--- src/Hole.cpp
+++ src/Hole.cpp
@@ -5,3 +5,9 @@ Hole::Hole(std::vector<Point> &holeArray): _holeArray(holeArray)
 {
 
 }
+
+
+std::vector<Point>& Hole::getPoints()
+{
+    return _holeArray;
+}
---
+++ src/Plot.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "Hole.hpp"
+#include "Figure.hpp"
+
+void plot(Figure &figure, Hole &hole);
